9a9671ea6e71403a67abc281e27a9a04b1ad4fba
Mahadi Hasan Biplob <biplobhasan547@gmail.com>
2021-08-30 13:53:01 +0600

    initial commit after completing full micro service for authentication

---
+++ drf_auth_micro_service/urls.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from django.conf.urls import url
+from .views import FacebookLogin
+from django.views.generic import TemplateView
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Auth API as a micro service with DRF",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('dj-rest-auth/facebook/', FacebookLogin.as_view(), name='fb_login'),
+    path('rest-auth/varification-email-sent/', TemplateView.as_view(), name='account_email_verification_sent'),
+    path('password-reset/confirm/<uidb64>/<token>/', TemplateView.as_view(), name='password_reset_confirm'),
+
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
